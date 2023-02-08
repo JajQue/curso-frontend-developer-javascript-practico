0f6e3c027f582b0def935feaf428c57fafb7fe4b
JaQue <jaque.code1@gmail.com>
2023-02-07 22:22:32 -0500

    Clase 19 Fusion del Menu Mobile

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
 
 menuEmail.addEventListener('click', toggleDesktopMenu);    // llamamos a la funcion con el evento click
+menuHamIcon.addEventListener('click', toggleMobileMenu);
 
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive'); // classList.toggle quita o pone la clase inactive dependiendo si la tiene o no la tiene
+}
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive'); 
 }
